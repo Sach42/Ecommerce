7ba52ae2527e3867eed53158e59950653dc4f6d6
Sachin <sachinkumar05282@gmail.com>
2024-02-09 10:40:39 +0530

    first full stack app

---
+++ front-end/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const navigate = useNavigate();
+    useEffect(() => {
+        const auth = localStorage.getItem('user');
+        if (auth) {
+            navigate("/")
+        }
+    }, [])
+    const handleLogin = async () => {
+        let result = await fetch('http://localhost:8080/login',{
+            method: 'post',
+            body: JSON.stringify({ email, password }),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result);
+        if (result.name) {
+            localStorage.setItem("user", JSON.stringify(result));
+            navigate("/")
+        } else {
+            alert("please enter correct details")
+        }
+    }
+
+    return (
+        <div className="login">
+            <input type="text" className="inputBox" placeholder="Enter Email"
+                onChange={(e) => setEmail(e.target.value)} value={email} />
+
+            <input type="password" className="inputBox" placeholder="Enter Password"
+                onChange={(e) => setPassword(e.target.value)} value={password} />
+
+            <button onClick={handleLogin} className="appButton" type="button">Login</button>
+
+        </div>
+    )
+}
+
+export default Login;
